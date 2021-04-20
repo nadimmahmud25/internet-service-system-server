f0591ca0ec27e7624ad1466d84ffc28238f8f9a5
nadimnil <nadimmahmudmy@gmail.com>
2021-04-20 23:11:55 +0600

    final

--- index.js
+++ index.js
@@ -2,147 +2,38 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs-extra');
 require('dotenv').config();
-const fileUpload = require('express-fileupload');
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w7kbq.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority";
 const ObjectId = require('mongodb').ObjectId; 
+const fileUpload = require('express-fileupload');
 
 const port = 5000;
 var app = express();
 app.use(express.json());
 app.use(cors());
-app.use(express.static('clientProjects'));
+app.use(express.static('services'));
 app.use(fileUpload());
 
 app.get('/', (req, res)=>{
     res.send("Hello World")
 });
 
+const user = process.env.DB_USER;
+const pass = process.env.DB_PASS;
+const dbname = process.env.DB_NAME;
+const adminTbl = process.env.DB_ADMIN_TBL;
+const orderTbl = process.env.DB_ORDER_TBL;
+const feedbackTbl = process.env.DB_FEEDBACK_TBL;
+const serviceTbl = process.env.DB_SERVICE_TBL;
+//const uri = "mongodb+srv://internet:internet1234@cluster0.w7kbq.mongodb.net/internetService?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${user}:${pass}@cluster0.w7kbq.mongodb.net/${dbname}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-    const adminCollection = client.db("wifiService").collection("admins");   
-    const orderCollection = client.db("wifiService").collection('orders');
-    const clientCommentsCollection = client.db("wifiService").collection('clientReview');
-    const serviceCollection = client.db("wifiService").collection('services');
-   
-
+    const adminCollection = client.db(dbname).collection(adminTbl);
+    const feedbackCollection = client.db(dbname).collection(feedbackTbl);
+    const orderCollection = client.db(dbname).collection(orderTbl);
+    const servicesCollection = client.db(dbname).collection(serviceTbl);
     
-    app.post('/addAdmin', (req, res)=>{
-        const admin = req.body;
-        // console.log(admin);
-        adminCollection.insertOne(admin)
-        .then(result =>{
-            // console.log(result);
-            res.send(result);
-        });
-    })
-
-    app.get('/admins', (req, res)=>{
-        const queryEmail = req.query.email;
-        // console.log(queryEmail);
-        adminCollection.find({email: queryEmail})
-        .toArray((err, documents) =>{
-            // console.log(documents);
-            res.send(documents);
-        });
-    })
-
-    // This work is for admin panel..
-    app.post('/addOrder', (req, res)=>{
-        const customerEmail = req.body.customerEmail;
-        const action = req.body.action;  
-        const actionBG = req.body.actionBG;  
-        const actionColor = req.body.actionColor;  
-        const name = req.body.name;  
-        const price = req.body.price;  
-        const email = req.body.email;  
-        const projectTitle = req.body.projectTitle;  
-        const projectDetails = req.body.projectDetails;
-        
-        
-        // console.log(action,actionBG,actionColor,name,email,projectTitle,projectDetails,price,file);
-       
-
-        orderCollection.insertOne({customerEmail,name,email,action,actionBG,actionColor,projectTitle,projectDetails,price})
-        then(result =>{
-            // console.log(result);
-         res.send(result.insertedCount > 0);
-
-                })
-            })
-    
-    //update action style
-    app.patch('/clients/:id', (req, res) =>{
-        const id = req.params.id;
-        // console.log(req.body.action,req.body.actionBG, req.body.actionColor)
-        // console.log(id);
-        orderCollection.updateOne({_id: ObjectId(id)},
-        {
-            $set: {action: req.body.action,actionBG: req.body.actionBG, actionColor: req.body.actionColor}
-        })
-        .then(result => {
-            res.send(result.modifiedCount > 0);
-        })
-    })
-
-   
-    app.post('/addServices', (req, res)=>{
-        const adminEmail = req.body.adminEmail;
-        const title = req.body.title;  
-        const description = req.body.description;
-        const price = req.body.price;
-        const file = req.files.file;
-
-        const filePath = `${__dirname}/addedServices/${file.name}`;
-            file.mv(filePath, (err) => {
-                if(err){
-                    console.log(err);
-                    res.status(500).send({msg: 'Failed to upload image'});
-                }
-                const newImg = fs.readFileSync(filePath);
-                const encImg = newImg.toString('base64');
-
-                const image = {
-                    contentType: file.mimetype,
-                    size: file.size,
-                    img: Buffer(encImg, 'base64')
-                };
-
-                serviceCollection.insertOne({adminEmail,title,description,price,image})
-                .then(result => {
-                    fs.remove(filePath, error => {
-                    if(error){
-                        console.log(error);
-                    }
-                    res.send(result.insertedCount > 0);
-                })
-            })
-        })
-    })
-app.delete('/deleteService/:id', (req, res) => {
-    serviceCollection.findOneAndDelete({_id: ObjectID (req.params.id)})
-    .then( (result) => {
-        res.send(result.deletedCount > 0);
-    })
-})
-
-    // This work is for customer
-    app.get('/services', (req, res) =>{
-        serviceCollection.find({})
-        .toArray( (err, documents) =>{
-            // console.log(documents);
-            res.send(documents); 
-        })
-    })
-
-    app.get('/services/:id', (req, res) =>{
-        const id = req.params.id;
-        serviceCollection.findOne({_id: ObjectId(id)})
-        .then(document => {
-            // console.log(document);  
-            res.send(document);
-        })
-    })
+    console.log('db connected');
 
     // post order to server
     app.post('/singleOrder', (req, res) => {
@@ -154,31 +45,114 @@ app.delete('/deleteService/:id', (req, res) => {
             })
     })
 
-        app.get('/clients/email', (req, res) =>{
-            const queryEmail = req.query.checkedEmail;
-            // console.log(queryEmail);
-            orderCollection.find({customerEmail: queryEmail})
-            .toArray( (err, documents) =>{
-            // console.log(documents);
-            res.send(documents); 
-            })
-        })
-
-        app.post('/clientComments', (req, res)=>{
-            const comment = req.body;
-            // console.log(comment);
-            clientCommentsCollection.insertOne(comment)
+    // client review order to server
+    app.post('/clientFeedback', (req, res) => {
+        const event = req.body;
+        feedbackCollection.insertOne(event)
             .then(result => {
-                res.send(result.insertedCount > 0);
+                console.log(result)
+                res.send(result)
             })
-        })
+    })
 
-        app.get('/clientComments', (req, res) =>{
-            clientCommentsCollection.find({})
-            .toArray( (err, documents) =>{
-            // console.log(documents);
-            res.send(documents); 
+    //add new services
+    app.post('/addAService', (req, res) => {
+        const file = req.files.file;
+        const title = req.body.title;
+        const price = req.body.price;
+        const description = req.body.description;
+        const newImg = file.data
+        const encImg = newImg.toString('base64')
+
+        var image = {
+            contentType: file.mimetype,
+            size: file.size,
+            img: Buffer.from(encImg, 'base64')
+        }
+
+        servicesCollection.insertOne({ title, price,description, image })
+            .then(result => {
+                res.send(result.insertedCount > 0)
             })
+    })
+
+    // add admin to server
+    app.post('/addAdmin', (req, res) => {
+        const event = req.body;
+        adminCollection.insertOne(event)
+            .then(result => {
+                console.log(result)
+                res.send(result)
+            })
+    })
+
+    //services
+    app.get('/services', (req, res) => {
+        servicesCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+    })
+    //Delete Service
+    app.delete('/deleteService/:id', (req, res) => {
+        servicesCollection.findOneAndDelete({_id: ObjectID (req.params.id)})
+        .then( (result) => {
+            res.send(result.deletedCount > 0);
         })
-    });
+    })
+    
+    //client feedback
+    app.get('/feedback', (req, res) => {
+        feedbackCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+    })
+
+    //order
+    app.get('/order', (req, res) => {
+        orderCollection.find({ email: req.query.email })
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+    })
+
+    //all admin
+    app.get('/allAdmin', (req, res) => {
+        adminCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+    })
+
+    //orderList
+    app.get('/orderList', (req, res) => {
+        orderCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+    })
+
+
+    //update Status
+    app.patch('/updateStatus', (req, res) => {
+        orderCollection.updateOne(
+            { _id: ObjectId(req.body.id) },
+            {
+                $set: { status: req.body.newStatus },
+                $currentDate: { "lastModified": true }
+            }
+        )
+            .then(result => {
+                res.send(result.modifiedCount > 0)
+            })
+    })
+
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
 app.listen(process.env.PORT || port)
